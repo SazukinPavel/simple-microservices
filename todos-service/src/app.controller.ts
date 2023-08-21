4523676efa50dc040f8351d3c3eba8a76ae0fd24
PavelSazukin <sazukinpavelal@gmail.com>
2023-08-22 00:48:42 +0400

    add rest like methods in todos service

--- todos-service/src/app.controller.ts
+++ todos-service/src/app.controller.ts
@@ -9,11 +9,21 @@ export class AppController {
 
   @MessagePattern({ cmd: 'get' })
   get() {
-    return this.appService.getTodos();
+    return this.appService.get();
   }
 
   @MessagePattern({ cmd: 'add' })
-  add(data: Todo) {
-    return this.appService.addTodo(data);
+  add(todo: Todo) {
+    return this.appService.add(todo);
+  }
+
+  @MessagePattern({ cmd: 'update' })
+  update(todo: Todo) {
+    return this.appService.update(todo);
+  }
+
+  @MessagePattern({ cmd: 'delete' })
+  delete(id: string) {
+    return this.appService.delete(id);
   }
 }
--- todos-service/src/app.service.ts
+++ todos-service/src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Todo } from './entities/todo.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOperator, Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
@@ -10,16 +10,26 @@ export class AppService {
     private todosRepository: Repository<Todo>,
   ) {}
 
-  async getTodos() {
-    return { result: await this.todosRepository.find() };
+  async get() {
+    return { result: true, data: await this.todosRepository.find() };
   }
 
-  async addTodo(todo: Todo) {
+  async add(todo: Todo) {
     const newTodo = this.todosRepository.create({ ...todo });
 
-    console.log(todo, newTodo);
+    const data = await this.todosRepository.save(newTodo);
 
-    await this.todosRepository.save(newTodo);
+    return { result: true, data };
+  }
+
+  async update(todo: Todo) {
+    const data = await this.todosRepository.update(todo.id, { ...todo });
+
+    return { result: true, data };
+  }
+
+  async delete(id: string) {
+    await this.todosRepository.delete(id);
 
     return { result: true };
   }
