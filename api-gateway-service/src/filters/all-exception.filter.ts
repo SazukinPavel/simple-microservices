58ba19f2f632e689099b0e39562953d22a5d8de7
PavelSazukin <sazukinpavelal@gmail.com>
2023-08-26 14:48:44 +0400

    add fiilter for trandorm rpc errors to normal http errors

---
+++ api-gateway-service/src/filters/all-exception.filter.ts
@@ -0,0 +1,42 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  InternalServerErrorException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: HttpException, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    let httpStatus = 500;
+    let responseBody = { message: '', statusCode: httpStatus };
+
+    if ((exception as unknown as any).response) {
+      const data = exception as unknown as any;
+      httpStatus = data.status;
+      responseBody = data.response;
+    } else if (exception?.message) {
+      exception = new HttpException(exception.message, HttpStatus.BAD_REQUEST);
+      httpStatus = 400;
+    } else {
+      exception = new InternalServerErrorException();
+      httpStatus = 500;
+    }
+
+    responseBody = responseBody.message
+      ? responseBody
+      : {
+          statusCode: httpStatus,
+          message: exception.message,
+        };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
--- api-gateway-service/src/main.ts
+++ api-gateway-service/src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LogsService } from './logs/logs.service';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,9 @@ async function bootstrap() {
   });
   app.useLogger(app.get(LogsService));
   app.useGlobalPipes(new ValidationPipe());
+
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   const configService = app.get(ConfigService);
   const port = configService.get('API_GATEWAY_PORT');
